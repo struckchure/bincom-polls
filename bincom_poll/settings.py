8a87d40ec8f512e57ecb560aabf903caf84c957d
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2021-11-18 10:25:51 +0100

    adds heroku configs

--- bincom_poll/settings.py
+++ bincom_poll/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOR = BASE_DIR.joinpath("static")
+
+django_heroku.settings(locals())
